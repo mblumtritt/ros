2e0675258bef20c1ea6c9f6e194be851254229d3
Mike Blumtritt <mike.blumtritt@injixo.com>
2015-10-13 23:15:54 +0200

    tidy up Regexp benchmark

--- benchmarks/regexp.rb
+++ benchmarks/regexp.rb
@@ -1,6 +1,14 @@
 require_relative '../lib/ruby_on_speed'
 
-A_SAMPLE_STRING = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.freeze
+A_SAMPLE_STRING = <<-EOS.freeze
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+EOS
+
 SAMPLE_REGEXP = /cillum/.freeze
 SAMPLE_NON_REGEXP = /ruby/.freeze
 

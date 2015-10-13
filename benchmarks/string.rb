87a8dfd9f61bf93750027f84eeb78e449c597cf2
Mike Blumtritt <mike.blumtritt@injixo.com>
2015-10-13 23:15:19 +0200

    tidy up String benchmarks

--- benchmarks/string.rb
+++ benchmarks/string.rb
@@ -1,6 +1,13 @@
 require_relative '../lib/ruby_on_speed'
 
-SAMPLE_STRING = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.freeze
+SAMPLE_STRING = <<-EOS.freeze
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+EOS
 
 RubyOnSpeed.check do
   code '"#{string}"', ->{ "#{SAMPLE_STRING}" }
@@ -23,41 +30,35 @@ RubyOnSpeed.check do
   REPLACEMENT = 'Ruby'
 
   code 'String#[string]=' do
-    str = SAMPLE_STRING.dup
+    str = "#{SAMPLE_STRING}"
     str[TO_FIND] = REPLACEMENT
     str
   end
 
   code 'String#sub(string)' do
-    str = SAMPLE_STRING.dup
+    str = "#{SAMPLE_STRING}"
     str.sub(TO_FIND, REPLACEMENT)
   end
 
   code 'String#sub!(string)' do
-    str = SAMPLE_STRING.dup
+    str = "#{SAMPLE_STRING}"
     str.sub!(TO_FIND, REPLACEMENT)
     str
   end
 
   code 'String#gsub(string)' do
-    str = SAMPLE_STRING.dup
+    str = "#{SAMPLE_STRING}"
     str.gsub(TO_FIND, REPLACEMENT)
   end
 
   code 'String#gsub!(string)' do
-    str = SAMPLE_STRING.dup
+    str = "#{SAMPLE_STRING}"
     str.gsub!(TO_FIND, REPLACEMENT)
     str
   end
 
-  code 'String#ubs' do
-    str = SAMPLE_STRING.dup
-    str.index(TO_FIND) and str[TO_FIND] = REPLACEMENT
-    str
-  end
-
   code 'String#use_index' do
-    str = SAMPLE_STRING.dup
+    str = "#{SAMPLE_STRING}"
     idx = str.index(TO_FIND) and str[idx, TO_FIND.size] = REPLACEMENT
     str
   end

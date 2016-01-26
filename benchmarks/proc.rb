62b6cb99ca456f91ff2b8ca0e8a52813c2dcb6af
Mike Blumtritt <mike.blumtritt@injixo.com>
2016-01-26 09:00:46 +0100

    Add tests for Proc#call vs Proc#[]

--- benchmarks/proc.rb
+++ benchmarks/proc.rb
@@ -1,19 +1,10 @@
 require_relative '../lib/ruby_on_speed'
 
 RubyOnSpeed.check do
-  def use_block(arg, &block)
-    block.call(arg)
+  TEST_PROC = proc do |a, b|
+    a + b
   end
 
-  def use_proc(arg)
-    Proc.new.call(arg)
-  end
-
-  def use_yield(arg)
-    yield arg
-  end
-
-  code '&block',   ->{ use_block(1){ |i| i } }
-  code 'Proc.new', ->{ use_proc(1){ |i| i } }
-  code 'yield',    ->{ use_yield(1){ |i| i } }
+  code 'Proc#call', ->{ TEST_PROC.call(1,2) }
+  code 'Proc#[]',   ->{ TEST_PROC[1,2] }
 end

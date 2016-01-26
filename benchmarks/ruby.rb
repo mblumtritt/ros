cbf45595d40ce4b5f3fd8495afcfefab410131b5
Mike Blumtritt <mike.blumtritt@injixo.com>
2016-01-26 06:54:06 +0100

    Move some tests to general Ruby section

--- benchmarks/ruby.rb
+++ benchmarks/ruby.rb
@@ -21,3 +21,29 @@ RubyOnSpeed.check do
     nil
   }
 end
+
+RubyOnSpeed.check do
+  def use_block(arg, &block)
+    block.call(arg)
+  end
+
+  def use_proc(arg)
+    Proc.new.call(arg)
+  end
+
+  def use_yield(arg)
+    yield arg
+  end
+
+  code '&block',   ->{ use_block(1){ |i| i } }
+  code 'Proc.new', ->{ use_proc(1){ |i| i } }
+  code 'yield',    ->{ use_yield(1){ |i| i } }
+end
+
+RubyOnSpeed.check do
+  RETURN_TRUE = ->{ true }
+  code 'if_end',      ->{ if RETURN_TRUE[]; 42; end }
+  code 'if modifier', ->{ 42 if RETURN_TRUE[] }
+  code 'ternary if',  ->{ RETURN_TRUE[] ? 42 : 21 }
+  code 'and',         ->{ RETURN_TRUE[] and 42 }
+end

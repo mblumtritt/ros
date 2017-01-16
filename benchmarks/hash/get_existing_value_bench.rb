c40412e81cbdfc2c6722963511e83935dbdd5714
Mike Blumtritt <mike.blumtritt@injixo.com>
2017-01-16 10:09:08 +0100

    Extend benchmark "Hash: get existing value"

--- benchmarks/hash/get_existing_value_bench.rb
+++ benchmarks/hash/get_existing_value_bench.rb
@@ -6,11 +6,14 @@ RubyOnSpeed.test 'Hash: get existing value' do
     Hash[ary.zip(ary)].freeze
   end
   sample_sym_hash = Hash[sample_hash.keys.map!(&:to_sym).zip(sample_hash.values)].freeze
+  sample_sym_identity_hash = Hash[sample_sym_hash].compare_by_identity
 
-  code '#[symbol]',      ->{ sample_sym_hash[:ru] }
-  code '#fetch(symbol)', ->{ sample_sym_hash.fetch(:ru) }
-  code '#[string]',      ->{ sample_hash['ru'.freeze] }
-  code '#fetch(string)', ->{ sample_hash.fetch('ru'.freeze) }
+  code '#[symbol]',          ->{ sample_sym_hash[:ru] }
+  code '#fetch(symbol)',     ->{ sample_sym_hash.fetch(:ru) }
+  code '#[symbol] (*)',      ->{ sample_sym_identity_hash[:ru] }
+  code '#fetch(symbol) (*)', ->{ sample_sym_identity_hash.fetch(:ru) }
+  code '#[string]',          ->{ sample_hash['ru'.freeze] }
+  code '#fetch(string)',     ->{ sample_hash.fetch('ru'.freeze) }
 end
 
 RubyOnSpeed.report! if $0 == __FILE__

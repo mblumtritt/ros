a0ae28d7dad490d14e5d2bf3c14e216ea6b47c27
Mike Blumtritt <mike.blumtritt@injixo.com>
2017-03-18 19:51:41 +0100

    Add test for proc/lambda call

---
+++ benchmarks/ruby/proc_or_lamda_bench.rb
@@ -0,0 +1,11 @@
+require_relative '../../lib/ruby_on_speed'
+
+RubyOnSpeed.test 'Ruby: proc/lambda call' do
+  sample_proc = proc{ |arg| 21 + arg }
+  sample_lambda = ->(arg){ 21 + arg }
+
+  code 'proc',     ->{ sample_proc.call(21) }
+  code 'lambda',   ->{ sample_lambda.call(21) }
+end
+
+RubyOnSpeed.report! if $0 == __FILE__

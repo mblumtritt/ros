689fbce24d1e7024afc2547cfda48168e0b52d4a
Mike Blumtritt <mike.blumtritt@injixo.com>
2017-03-18 19:51:54 +0100

    Add tests for deleting chars from String

---
+++ benchmarks/string/remove_chars.rb
@@ -0,0 +1,19 @@
+require_relative '../../lib/ruby_on_speed'
+
+RubyOnSpeed.test 'Remove many chars from String' do
+  sample_string = <<-EOS.freeze
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  EOS
+
+  REGEX = / |e/
+  code '#gsub',   ->{ sample_string.gsub(REGEX, ''.freeze) }
+  code '#tr',     ->{ sample_string.tr(' e'.freeze, ''.freeze) }
+  code '#delete', ->{ sample_string.delete(' e'.freeze) }
+end
+
+RubyOnSpeed.report! if $0 == __FILE__

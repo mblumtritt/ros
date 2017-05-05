c164c77d0155b163965c31e8584cf662216a6054
Mike Blumtritt <mike.blumtritt@injixo.com>
2017-05-05 09:55:43 +0200

    Add "Struct vs. Hash: access"

---
+++ benchmarks/struct/vs_hash_bench.rb
@@ -0,0 +1,20 @@
+require_relative '../../lib/ruby_on_speed'
+
+RubyOnSpeed.test 'Struct vs. Hash: access' do
+  struct = Struct.new(:a, :b, :c, :d).new
+  hash = {a: nil, b: nil, c: nil, d: nil}
+
+  code 'struct#' do
+    struct.a = struct.b = 1
+    struct.c, struct.d = struct.a, struct.b
+    struct.a += struct.b
+  end
+
+  code 'hash#' do
+    hash[:a] = hash[:b] = 1
+    hash[:c], hash[:d] = hash[:a], hash[:b]
+    hash[:a] += hash[:b]
+  end
+end
+
+RubyOnSpeed.report! if __FILE__ == $PROGRAM_NAME

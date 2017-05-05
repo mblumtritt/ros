08ee389311ae72c2472a72cb7eb9bf7acc47c1ac
Mike Blumtritt <mike.blumtritt@injixo.com>
2017-05-05 09:55:25 +0200

    Extend "Array: concatinate"

--- benchmarks/array/concatinate_bench.rb
+++ benchmarks/array/concatinate_bench.rb
@@ -3,8 +3,9 @@ require_relative '../../lib/ruby_on_speed'
 RubyOnSpeed.test 'Array: concatinate' do
   sample_array = Array.new(1000, &:itself).freeze
 
-  code '#+',      ->{ Array.new(sample_array) + sample_array }
-  code '#concat', ->{ Array.new(sample_array).concat(sample_array) }
+  code '#+',           ->{ Array.new(sample_array) + sample_array }
+  code '#concat',      ->{ Array.new(sample_array).concat(sample_array) }
+  code '#[]#flatten!', ->{ [sample_array, sample_array].flatten!(1) }
 end
 
 RubyOnSpeed.report! if __FILE__ == $PROGRAM_NAME

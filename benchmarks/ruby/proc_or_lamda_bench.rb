a7eec0067905c6827c4fa5ef19c8ea22da36cc36
Mike Blumtritt <mike.blumtritt@injixo.com>
2017-05-05 09:56:13 +0200

    Add benchmark for method call
    „Ruby: proc/lambda/method call“

--- benchmarks/ruby/proc_or_lamda_bench.rb
+++ benchmarks/ruby/proc_or_lamda_bench.rb
@@ -1,11 +1,18 @@
 require_relative '../../lib/ruby_on_speed'
 
-RubyOnSpeed.test 'Ruby: proc/lambda call' do
+RubyOnSpeed.test 'Ruby: proc/lambda/method call' do
   sample_proc = proc{ |arg| 21 + arg }
   sample_lambda = ->(arg){ 21 + arg }
+  sample_method = Class.new do
+    def add(arg)
+      21 + arg
+    end
+  end.new.method(:add)
 
   code 'proc',     ->{ sample_proc.call(21) }
   code 'lambda',   ->{ sample_lambda.call(21) }
+  code 'method',   ->{ sample_method.call(21) }
 end
 
+RubyOnSpeed.test! if __FILE__ == $PROGRAM_NAME
 RubyOnSpeed.report! if __FILE__ == $PROGRAM_NAME

a45ef4c78911d225c4dd23c778982ebdb0c8c38a
Mike Blumtritt <mike.blumtritt@injixo.com>
2017-05-16 11:01:40 +0200

    Add benchmark for 'Array: subset included'

---
+++ benchmarks/array/subset_included_bench.rb
@@ -0,0 +1,14 @@
+require_relative '../../lib/ruby_on_speed'
+
+RubyOnSpeed.test 'Array: subset included' do
+  sample_all = [*(1..500)].freeze
+  sample_subset = [*(400..500)].freeze
+
+  code '#-#empty?',     ->{ (sample_subset - sample_all).empty? }
+  code '#&',            ->{ (sample_subset & sample_all) == sample_subset }
+  code '#all?#include?', ->{ sample_subset.all?{ |e| sample_all.include?(e) } }
+  code '#all?#index',    ->{ sample_subset.all?{ |e| sample_all.index(e) } }
+end
+
+RubyOnSpeed.test! if __FILE__ == $PROGRAM_NAME
+RubyOnSpeed.report! if __FILE__ == $PROGRAM_NAME

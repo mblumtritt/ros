3028276629ca134c13657d29baed75787783a010
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2017-06-07 21:46:30 +0200

    Add benchmark 'Hash: merge vs double splat'

---
+++ benchmarks/hash/merge_bench.rb
@@ -0,0 +1,10 @@
+require_relative '../../lib/ruby_on_speed'
+
+RubyOnSpeed.test 'Hash: merge vs double splat' do
+  sample = Hash[('a'..'z').to_a.map!{ |c| [c.to_sym, c] }].freeze # does not work with Strings as keys?!
+
+  code '#merge', ->{ {a: 1, b: 2, 1 => :a, 2 => :b}.merge(sample) }
+  code '#**',    ->{ {a: 1, b: 2, 1 => :a, 2 => :b, **sample} }
+end
+
+RubyOnSpeed.report! if __FILE__ == $PROGRAM_NAME

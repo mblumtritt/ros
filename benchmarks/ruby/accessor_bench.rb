e9e7bbdd74fc58dd4fff27aa776b77fdc6c152a4
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-01-26 18:27:04 +0100

    Add accessor benchmark

---
+++ benchmarks/ruby/accessor_bench.rb
@@ -0,0 +1,26 @@
+require_relative '../../lib/ruby_on_speed'
+
+RubyOnSpeed.test 'Ruby: getter/setter vs. accessor' do
+  class Foo
+    attr_accessor :bar
+
+    def initialize
+      @bar = @baz = 1
+    end
+
+    def baz
+      @baz
+    end
+
+    def baz=(value)
+      @baz = value
+    end
+  end
+
+  FooInstance = Foo.new
+
+  code 'accessor',      ->{ FooInstance.bar = FooInstance.bar }
+  code 'getter/setter', ->{ FooInstance.baz = FooInstance.baz }
+end
+
+RubyOnSpeed.report! if __FILE__ == $PROGRAM_NAME

06fdd8c8fc980a62a9ffe892695f458d44083a42
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-02-04 18:24:01 +0100

    Add benchmark for calls on Struct members

---
+++ benchmarks/struct/member_access_bench.rb
@@ -0,0 +1,17 @@
+require_relative '../../lib/ruby_on_speed'
+
+RubyOnSpeed.test 'Struct member access' do
+  struct = Struct.new(:a, :b, :c, :d).new
+
+  code '#method' do
+    struct.a = struct.b = 1
+    struct.a += struct.b
+  end
+
+  code '#[]' do
+    struct[:a] = struct[:b] = 1
+    struct[:a] += struct[:b]
+  end
+end
+
+RubyOnSpeed.report! if __FILE__ == $PROGRAM_NAME

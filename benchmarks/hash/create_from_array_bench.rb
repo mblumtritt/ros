1aa29fb9826f4f854a257abea92523e88d2a3c96
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-03-27 16:57:45 +0200

    Add „Hash: create from Array“ benchmark

---
+++ benchmarks/hash/create_from_array_bench.rb
@@ -0,0 +1,26 @@
+require_relative '../../lib/ruby_on_speed'
+
+RubyOnSpeed.test 'Hash: create from Array' do
+  TestItem = Struct.new(:id, :name)
+
+  sample_array = Array.new(1000) do |i|
+    TestItem.new(i + 1, "name-#{i}")
+  end.freeze
+
+  code 'Hash.[]' do
+    Hash[sample_array.map{ |i| [i.id, i] }]
+  end
+
+  code 'Array.to_h' do
+    sample_array.map{ |i| [i.id, i] }.to_h
+  end
+
+  code 'fill' do
+    ret = {}
+    sample_array.each{ |i| ret[i.id] = i }
+    ret
+  end
+end
+
+RubyOnSpeed.test! if __FILE__ == $PROGRAM_NAME
+RubyOnSpeed.report! if __FILE__ == $PROGRAM_NAME

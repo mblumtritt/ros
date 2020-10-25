5e8058487ce18526147bc2c35247f33deb6a0bcd
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2020-10-25 08:54:08 +0100

    Add bench for entity request

---
+++ benchmarks/ruby/entity_bench.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/ruby-on-speed'
+
+RubyOnSpeed.test 'Ruby: request_entity vs. check_method' do
+  EntityFooInstance =
+    Class.new do
+      attr_reader :entity
+
+      def initialize
+        @entity = true
+      end
+
+      def entity?
+        true
+      end
+
+      def foo; end
+    end.new
+
+  code 'accessor',    -> { EntityFooInstance.entity }
+  code 'call',        -> { EntityFooInstance.entity? }
+  code 'defined?',    -> { defined?(EntityFooInstance.foo) }
+  code 'respond_to?', -> { EntityFooInstance.respond_to?(:foo) }
+end
+
+RubyOnSpeed.report! if __FILE__ == $PROGRAM_NAME

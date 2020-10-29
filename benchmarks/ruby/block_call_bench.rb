898138e0dbbad96946ad8e3f54a747e91e22e0f9
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2020-10-29 21:36:46 +0100

    Actualize benchmark 'Ruby: block call'
    - since Ruby 2.7 `proc` is deprecated

--- benchmarks/ruby/block_call_bench.rb
+++ benchmarks/ruby/block_call_bench.rb
@@ -3,31 +3,21 @@
 require_relative '../../lib/ruby-on-speed'
 
 RubyOnSpeed.test 'Ruby: block call' do
-  def block_call(arg, &block)
-    block.call(arg)
+  def use_yield(arg)
+    yield(arg)
   end
 
-  def proc_call(arg)
-    proc.call(arg)
+  def block_call(arg, &block)
+    block.call(arg)
   end
 
   def block_idx(arg, &block)
     block[arg]
   end
 
-  def proc_idx(arg)
-    proc[arg]
-  end
-
-  def use_yield(arg)
-    yield(arg)
-  end
-
-  code 'yield',     ->{ use_yield(1){ |i| i + i } }
-  code '&block',    ->{ block_call(1){ |i| i + i } }
-  code 'proc',      ->{ proc_call(1){ |i| i + i } }
-  code '&block#[]', ->{ block_idx(1){ |i| i + i } }
-  code 'proc#[]',   ->{ proc_idx(1){ |i| i + i } }
+  code 'yield',       ->{ use_yield(1){ |i| i + i } }
+  code '&block#call', ->{ block_call(1){ |i| i + i } }
+  code '&block#[]',   ->{ block_idx(1){ |i| i + i } }
 end
 
 RubyOnSpeed.report! if __FILE__ == $PROGRAM_NAME

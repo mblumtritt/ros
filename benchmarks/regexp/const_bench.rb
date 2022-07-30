6ba7cfe206e651c3ec1e3ca56e09895301718177
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2022-07-30 21:18:16 +0200

    Re-organize Regexp benchmarks

--- benchmarks/regexp/const_bench.rb
+++ benchmarks/regexp/const_bench.rb
@@ -2,20 +2,20 @@
 
 require_relative '../../lib/ruby-on-speed'
 
-RubyOnSpeed.test 'Regexp: const vs. inline' do
-  sample_regexp = sample_string = <<-SAMPLE_STRING
+RubyOnSpeed.test 'Regexp:match - find Match of an Regexp' do
+  text = <<-TEXT
     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
     non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  SAMPLE_STRING
+  TEXT
 
-  TEST_RE_1 = /cillum\s/
+  Sample = /cillum\s/.freeze
 
-  code 'inline', -> { /cillum\s/.match(sample_string) }
-  code 'const', -> { TEST_RE_1.match(sample_string) }
+  code 'inline', -> { /cillum\s/.match(text) }
+  code 'const', -> { Sample.match(text) }
 end
 
 RubyOnSpeed.report! if __FILE__ == Process.argv0
--- benchmarks/regexp/matche_bench.rb
+++ benchmarks/regexp/matche_bench.rb
@@ -2,24 +2,25 @@
 
 require_relative '../../lib/ruby-on-speed'
 
-RubyOnSpeed.test 'Regexp: matches' do
+RubyOnSpeed.test 'Regexp:match - test if a Regexp matches' do
   has_truthy_results!
-  sample_regexp = /cillum/
-  sample_string = <<-SAMPLE_STRING
+
+  sample = /cillum/
+  text = <<-TEXT
     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  SAMPLE_STRING
+  TEXT
 
-  code '#match?', -> { sample_regexp.match?(sample_string) }
-  code '#===', -> { sample_regexp === sample_string }
-  code '#=~', -> { sample_regexp =~ sample_string }
-  code 'string#=~', -> { sample_string =~ sample_regexp }
-  code '#match', -> { sample_regexp.match(sample_string) }
-  code 'string#match', -> { sample_string.match(sample_regexp) }
+  code '#match?', -> { sample.match?(text) }
+  code '#===', -> { sample === text }
+  code '#=~', -> { sample =~ text }
+  code 'string#=~', -> { text =~ sample }
+  code '#match', -> { sample.match(text) }
+  code 'string#match', -> { text.match(sample) }
 end
 
 RubyOnSpeed.report! if __FILE__ == Process.argv0

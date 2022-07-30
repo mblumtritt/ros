f5063f531a5ba8e25a03ad7e7bd79a8e88bd49ea
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2022-07-30 21:08:47 +0200

    Re-organize Proc benchmarks

--- benchmarks/proc/call_bench.rb
+++ benchmarks/proc/call_bench.rb
@@ -2,13 +2,13 @@
 
 require_relative '../../lib/ruby-on-speed'
 
-RubyOnSpeed.test 'Proc: call' do
-  sample_proc = proc { |x, y| x + y }
+RubyOnSpeed.test 'Proc:call - call a Proc with parameters' do
+  sample = proc { |x, y| x + y }
 
-  code '#call', -> { sample_proc.call(28, 14) }
-  code '#[]', -> { sample_proc[28, 14] }
-  code '#()', -> { sample_proc.(28, 14) }
-  code '#yield', -> { sample_proc.yield(28, 14) }
+  code '#call', -> { sample.call(28, 14) }
+  code '#[]', -> { sample[28, 14] }
+  code '#()', -> { sample.(28, 14) }
+  code '#yield', -> { sample.yield(28, 14) }
 end
 
 RubyOnSpeed.report! if __FILE__ == Process.argv0

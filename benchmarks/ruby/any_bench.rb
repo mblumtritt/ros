437bdfccc09da78d9dc269e7a5fb1ed2290abb3d
Mike Blumtritt <mike.blumtritt@pm.me>
2023-04-11 22:07:18 +0200

    Add benchmark for "#any"

---
+++ benchmarks/ruby/any_bench.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/ruby-on-speed'
+
+RubyOnSpeed.test 'Ruby:any - single test vs. any' do
+  def test_dedicated
+    1.even? || 3.even? || 5.even? || 7.even?
+  end
+
+  def test_any(*args)
+    args.any?(&:even?)
+  end
+
+  code 'dedicated' do
+    test_dedicated
+  end
+
+  code 'any' do
+    test_any(1, 3, 5, 7)
+  end
+end
+
+RubyOnSpeed.report! __FILE__

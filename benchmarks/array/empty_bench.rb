045a9a47b46ca15da29babd67c3547db222168cb
Mike Blumtritt <mike.blumtritt@pm.me>
2023-12-19 17:04:33 +0100

    Add a benchmark about the use of an empty Array

---
+++ benchmarks/array/empty_bench.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/ruby-on-speed'
+
+RubyOnSpeed.benchmark 'Array:empty - use of an empty Array' do
+  sample =
+    Class
+      .new do
+        EMPTY = [].freeze
+
+        def as_const
+          EMPTY
+        end
+
+        def as_create
+          []
+        end
+      end
+      .new
+
+  code 'const' do
+    sample.as_const
+  end
+
+  code 'create' do
+    sample.as_create
+  end
+end
+
+RubyOnSpeed.report! __FILE__

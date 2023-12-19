fbfcdd6980272e9b6756b14fd9d3d315b9ebc5b5
Mike Blumtritt <mike.blumtritt@pm.me>
2023-12-19 17:03:43 +0100

    Add a benchmark to count number of lines in a text

---
+++ benchmarks/string/lines_count_bench.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/ruby-on-speed'
+
+RubyOnSpeed.benchmark('String:lines_count - count number of lines') do
+  sample = fixture(:lorem_ipsum)
+
+  code '#lines#size' do
+    sample.lines.size
+  end
+
+  code '#each_line#count' do
+    sample.each_line.count
+  end
+
+  code '#count' do
+    sample.count($/)
+  end
+end
+
+RubyOnSpeed.report! __FILE__

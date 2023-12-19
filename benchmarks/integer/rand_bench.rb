2b75e1753dda98e33949b7c523f04f00f1c31935
Mike Blumtritt <mike.blumtritt@pm.me>
2023-12-19 17:01:11 +0100

    Add benchmark to check a 50% chance

---
+++ benchmarks/integer/rand_bench.rb
@@ -0,0 +1,17 @@
+require_relative '../../lib/ruby-on-speed'
+
+RubyOnSpeed.benchmark 'Integer:rand - check 50% chance' do
+  code '< 50' do
+    rand(100) < 50
+  end
+
+  code '2#even?' do
+    rand(2).even?
+  end
+
+  code '100#even?' do
+    rand(100).even?
+  end
+end
+
+RubyOnSpeed.report! __FILE__
